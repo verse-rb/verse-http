714993f425758d696e97f1f2a06f943381a69109
Yacine <yacine.petitprez@oivan.com>
2023-06-21 11:49:37 +0700

    Testing the renderers

--- spec/verse/http/renderers/binary_renderer_spec.rb
+++ spec/verse/http/renderers/binary_renderer_spec.rb
@@ -13,5 +13,19 @@ RSpec.describe Verse::Http::Renderers::BinaryRenderer do
 
       expect(renderer.render(result, ctx)).to eq io
     end
+
+    it "allows to set custom content type" do
+      result = double("result")
+      io = StringIO.new("hello world")
+      expect(result).to receive(:body).and_return(io)
+
+      ctx = double("ctx")
+      expect(ctx).to receive(:content_type).with("image/png")
+      expect(ctx).to receive(:attachment).with(String)
+
+      renderer.content_type = "image/png"
+
+      expect(renderer.render(result, ctx)).to eq io
+    end
   end
 end
