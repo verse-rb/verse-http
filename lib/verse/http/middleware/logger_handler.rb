575128be3079a526aca95ba708ca893900353e25
Yacine <anykeyh@gmail.com>
2024-01-24 14:05:47 +0700

    Fix rescue on logger handler

--- lib/verse/http/middleware/logger_handler.rb
+++ lib/verse/http/middleware/logger_handler.rb
@@ -27,7 +27,7 @@ module Verse
           end
 
           output
-        rescue RuntimeError => e
+        rescue => e
           time = Process.clock_gettime(Process::CLOCK_MONOTONIC) - time
           Verse.logger.warn(e)
 
