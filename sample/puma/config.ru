f96ae0f9a117a8ca5dea014ac3025cb526b9599a
Yacine Petitprez <anykeyh@gmail.com>
2025-07-26 12:42:37 +0700

    remove sample (unused)

--- sample/puma/config.ru
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-require File.expand_path("config/boot.rb", __dir__)
-
-run Verse::Http::Server
--- sample/puma/config/routes.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Verse.on_boot do
-  # Include the exposition after Verse has been initialized
-  Expo.register
-end
